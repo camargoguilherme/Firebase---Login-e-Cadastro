406fc3beebd6509864d19ee8af5eec0cd5d4df69
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-07-08 16:52:40 -0300

    Adicionando Referencias

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,11 +1,13 @@
 function getConfig(){
 	config = {
-		apiKey: "*****",
-		databaseURL: "*****",
-		projectId: "*****",
-		storageBucket: "*****",
-		messagingSenderId: "*****",
-		appId: "*****",
+		apiKey: "AIzaSyAmSnzlRjI8BussfEmdHMQFYwulGfL-pOI",
+    authDomain: "signin-signup-teste.firebaseapp.com",
+    databaseURL: "https://signin-signup-teste.firebaseio.com",
+    projectId: "signin-signup-teste",
+    storageBucket: "",
+    messagingSenderId: "453055694400",
+    appId: "1:453055694400:web:aebfa6bd4348982b"
+
 	};
 	return config;
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,36 @@ $( document ).ready(function() {
   firebase.initializeApp(getConfig());
 
   $('.tab a').on('click', function (e) {
-    
+    // https://developer.mozilla.org/pt-BR/docs/Web/API/Event/preventDefault
+    // https://www.geeksforgeeks.org/preventdefault-event-method/
     e.preventDefault();
     
     $(this).parent().addClass('active');
+
+    // https://www.w3schools.com/jquery/traversing_siblings.asp
     $(this).parent().siblings().removeClass('active');
     
-    target = $(this).attr('href');
 
+    // verifica tab ativa na lista pelo href da ancora (#login / #signup)
+    target = $(this).attr('href');
+    
+    // Esconde div com o id que nao tenha o valor de target (#login / #signup)
+    // 
+    // https://api.jquery.com/hide/
     $('.tab-content > div').not(target).hide();
     
+    // https://api.jquery.com/fadeIn/
+    // https://www.w3schools.com/jquery/eff_fadein.asp
     $(target).fadeIn(500);
     
   });
-
+  
+  
+  
+  // https://stackoverflow.com/questions/16345870/keydown-keyup-events-for-specific-keys
   $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
-    var $this = $(this),
-        label = $this.prev('label');
+    var $this = $(this),  label = $this.prev('label');
   
       if (e.type === 'keyup') {
         if ($this.val() === '') {
@@ -54,12 +66,17 @@ $( document ).ready(function() {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function({user}){
       console.log('user', user.uid)
-      alert('user id', user.uid)
+      alert('user id' + user.uid)
     })
     .catch(function(error) {
       // Handle Errors here.
+      // https://firebase.google.com/docs/auth/web/password-auth
       var errorCode = error.code;
+      //"auth/operation-not-allowed"
       var errorMessage = error.message;
+      /*
+      "The given sign-in provider is disabled for this Firebase project. Enable it in the Firebase console, under the sign-in method tab of the Auth section."
+      */
       // ...
       console.log(error)
     });
@@ -76,7 +93,7 @@ $( document ).ready(function() {
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function({user}){
       console.log('user', user.uid)
-      alert('user id', user.uid)
+      alert('user id' + user.uid)
     })
     .catch(function(error) {
       // Handle Errors here.
